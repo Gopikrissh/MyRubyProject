461a2b95e9f38bc66eb85b0d37e747bc8a47dedf
Alan <ecewebarch@gmail.com>
2015-12-01 16:39:29 -0700

    small mandrill update

--- db/migrate/20151201222759_add_confirmable_to_devise.rb
+++ db/migrate/20151201222759_add_confirmable_to_devise.rb
@@ -3,8 +3,8 @@ def up
     add_column :profiles, :confirmation_token, :string
     add_column :profiles, :confirmed_at, :datetime
     add_column :profiles, :confirmation_sent_at, :datetime
-    # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
-    add_index :profiles, :confirmation_token, unique: true
+    add_column :profiles, :unconfirmed_email, :string # Only if using reconfirmable
+    add_index  :profiles, :confirmation_token, unique: true
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
