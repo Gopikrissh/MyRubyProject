37264ba02fe6656808348083f2c219a108ba170f
Alan <ecewebarch@gmail.com>
2015-12-01 20:15:13 -0700

    small change on database and schema

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20151201222759) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "profiles", ["confirmation_token"], name: "index_profiles_on_confirmation_token", unique: true, using: :btree
